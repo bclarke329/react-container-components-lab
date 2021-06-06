4ed75b5257a674cd706ab35c7b3743bc1ea36ad4
bclarke329 <bmendoza329@hotmail.com>
2021-06-06 01:19:43 -0500

    commit tree 1c61047c9c2e7959fa6640ba66437503dbb946fe

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,9 @@
 // Code MovieReviews Here
+import React from 'react'
+import LatestMovieReviewsContainer from './LatestMovieReviewsContainer'
+
+const Review = ({}) => {
+    
+}
+
+export default LatestMovieReviewsContainer;
